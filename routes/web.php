be1fc6c44b2d0eb182bcad28d832fef997e4eb8e
gwelle <guillaume.welle.sio@gmail.com>
2025-04-11 13:18:36 +0200

    first views with h1 and articles list

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $articles = [
+        [
+            'title' => 'Article 1',
+            'content' => 'This is the content of article 1.',
+        ],
+        [
+            'title' => 'Article 2',
+            'content' => 'This is the content of article 2.',
+        ],
+        [
+            'title' => 'Article 3',
+            'content' => 'This is the content of article 3.',
+        ],
+    ];
+
+    return view('home', ['articles' => $articles]);
+});
+
+Route::get('hello-world', function(){
+    return view('hello-world');
 });
